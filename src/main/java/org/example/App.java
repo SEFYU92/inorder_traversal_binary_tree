7661ede6295ba64c3de0e400362b669ce42bb879
El Korji Youssef <youssefelkorji92@gmail.com>
2023-04-18 01:11:59 +0200

    preorder reading

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,9 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -9,6 +11,8 @@ import static java.util.Collections.reverse;
 import static java.util.stream.Collectors.toList;
 
 /*
+    input : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
                    7
                 /    \
                4      9
@@ -16,21 +20,29 @@ import static java.util.stream.Collectors.toList;
             2     6 8  10
            / \   /
           1   3 5
-     */
+
+    output : [7, 4, 9, 2, 6, 8, 10, 1, 3, 5]
+*/
 public class App {
     public static void main(String[] args) {
         var nodesStack = new Stack<Integer>();
-        var nodesList = Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).collect(Collectors.collectingAndThen(toList(), App::reverseList));
+        Integer[] integers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        var nodesList = Stream.of(integers).collect(Collectors.collectingAndThen(toList(), App::reverseList));
         nodesStack.addAll(nodesList);
 
         var depth = log2(nodesStack.size());
+        var maxDepth = new AtomicInteger(depth);
         System.out.println(depth);
 
-        var numberOfMaxDepthNodes = nodesStack.size() - (Math.pow(2, depth) - 1);
+        var numberOfMaxDepthNodes = new AtomicInteger((int) (nodesStack.size() - (Math.pow(2, depth) - 1)));
         System.out.println(numberOfMaxDepthNodes);
 
         var node = new Node();
-        node.buildTree(nodesStack, depth, depth);
+        node.buildInorderTree(nodesStack, depth, maxDepth, numberOfMaxDepthNodes);
+        var root = node.getRoot();
+        var preorderTreeList = new ArrayList<Integer>(10);
+        System.out.println("reading");
+        root.readPreorderTree(preorderTreeList,10);
     }
 
     private static List<Integer> reverseList(List<Integer> integers) {
--- src/main/java/org/example/Node.java
+++ src/main/java/org/example/Node.java
@@ -1,6 +1,8 @@
 package org.example;
 
+import java.util.List;
 import java.util.Stack;
+import java.util.concurrent.atomic.AtomicInteger;
 
 class Node {
     int value;
@@ -8,29 +10,68 @@ class Node {
     Node right;
     Node left;
 
-    void buildTree(Stack<Integer> nodes, int currentDepth, int maxDepth) {
-        if (left == null && currentDepth < maxDepth) {
+    void buildInorderTree(Stack<Integer> nodes, int currentDepth, AtomicInteger maxDepth, AtomicInteger lastLevelNodesNumber) {
+        if (left == null && currentDepth < maxDepth.get()) {
             System.out.println("left child");
             left = new Node();
             left.parent = this;
-            left.buildTree(nodes, currentDepth + 1, maxDepth);
+            left.buildInorderTree(nodes, currentDepth + 1, maxDepth, lastLevelNodesNumber);
         }
 
         value = nodes.pop();
+        if(currentDepth == maxDepth.get()){
+            System.out.println("max depth reached");
+            if (lastLevelNodesNumber.decrementAndGet() == 0) {
+                System.out.println("all max depth nodes reached, change max depth");
+                maxDepth.decrementAndGet();
+            }
+
+        }
         System.out.println("node " + value + " depth " + currentDepth);
 
-        if (right == null && currentDepth < maxDepth) {
+        if (right == null && currentDepth < maxDepth.get()) {
             System.out.println("right child");
             right = new Node();
             right.parent = this;
-            right.buildTree(nodes, currentDepth + 1, maxDepth);
+            right.buildInorderTree(nodes, currentDepth + 1, maxDepth, lastLevelNodesNumber);
         }
 
         if (parent == null && currentDepth > 0) {
             System.out.println("parent");
             parent = new Node();
             parent.left = this;
-            parent.buildTree(nodes, currentDepth - 1, maxDepth);
+            parent.buildInorderTree(nodes, currentDepth - 1, maxDepth, lastLevelNodesNumber);
+        }
+    }
+
+    public Node getRoot() {
+        if (this.parent != null) {
+            return this.parent.getRoot();
+        } else {
+            return this;
+        }
+    }
+
+/*
+                   7
+                /    \
+               4      9
+             /   \   / \
+            2     6 8  10
+           / \   /
+          1   3 5
+*/
+    public void readPreorderTree(List<Integer> preorderTreeList, int size) {
+        System.out.println("value : "+this.value);
+        preorderTreeList.add(this.value);
+        if (preorderTreeList.size() == size) {
+            return;
+        }
+        if (this.left != null) {
+            this.left.readPreorderTree(preorderTreeList,size);
+        }
+        if (this.right != null) {
+            this.right.readPreorderTree(preorderTreeList,size);
         }
     }
 }
